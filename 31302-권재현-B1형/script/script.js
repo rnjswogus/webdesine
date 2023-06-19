991e5abe9dd26c703d1e1785dd06f5ad3ae4bc09
권재현 <121943938+rnjswogus@users.noreply.github.com>
2023-06-19 12:00:50 +0900

    Delete 31302-권재현-B1형 directory

--- 31302-권재현-B1형/script/script.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function(){
-    $(".main>li").mouseover(function(){
-        $(".sub").stop().slideDown()
-    })
-    $(".main>li").mouseout(function(){
-        $(".sub").stop().slideUp()
-    })
-})
